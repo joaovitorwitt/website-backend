75dbbec7a3527a89b1774cd300e774380542f13a
João Vitor Witt <joaovitorwitt2004@gmail.com>
2024-07-13 17:11:02 -0300

    Fix: stuff

--- core/requests.py
+++ core/requests.py
@@ -1,5 +1,5 @@
 
-
+import http.server
 
 
 class Requests:
---
+++ core/server.py
@@ -0,0 +1,18 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+HOST = ""
+
+class MainHTTPHandler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        self.path
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+
+
+        self.wfile.write(bytes("<h1>asdasd</h1>"))
+
+
+
+server = HTTPServer(())

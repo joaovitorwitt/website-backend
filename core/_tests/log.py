cf8cc66aa711fb4d35ecc7648c29ab6fc005481f
João Vitor Witt <joaovitorwitt2004@gmail.com>
2024-09-22 16:21:05 -0300

    stuff

---
+++ core/_tests/log.py
@@ -0,0 +1,12 @@
+import unittest
+
+from unittest import TestCase, mock
+
+from core.log import Log
+
+class LogTestCase(TestCase):
+
+    pass
+
+if __name__ == '__main__':
+    unittest.main()
--- core/tests.py
+++ core/tests.py
@@ -3,3 +3,4 @@ from core._tests.string import StringTestCase
 from core._tests.criptography import CriptographyTestCase
 from core._tests.serialize import SerializeTestCase
 from core._tests.utils import UtilsTestCase
+from core._tests.log import LogTestCase

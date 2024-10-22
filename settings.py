4247721ff6d9ffb715fb4516c6eed7a2bfcd323a
João Vitor Witt <joaovitorwitt2004@gmail.com>
2024-10-21 21:37:21 -0300

    added CORS stuff

--- settings.py
+++ settings.py
@@ -40,4 +40,4 @@ STANDARD_LOG_DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
 ###################################################
 #  cors
 ###################################################
-CORS_ALLOWED_ORIGINS = ["*", "joaovitorwitt.com", "www.joaovitorwitt.com", "localhost:3000", "127.0.0.1", "https://portfolio-backend-fdxe.onrender.com"]
+CORS_ALLOWED_ORIGINS = ["*", "joaovitorwitt.com", "www.joaovitorwitt.com", "localhost:3000", "127.0.0.1", "https://joaovitorwitt.pythonanywhere.com"]

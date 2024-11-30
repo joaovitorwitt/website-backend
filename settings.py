c40a89eb6f70659d244c6ec90593cc6196f062e9
João Vitor Witt <joaovitorwitt2004@gmail.com>
2024-11-30 11:17:44 -0300

    remove unused variables in settings

--- settings.py
+++ settings.py
@@ -1,29 +1,13 @@
 # pylint: disable=line-too-long
-PYTHON_VERSION='3.11.7'
-
-# Database configuration
-DATABASE_NAME='website_backend_0fvg'
-DATABASE_USER='website_backend_0fvg_user'
-DATABASE_PASSWORD='-4DfgdeGBDFDFg*d2eG2cCGF*6F2*bE2'
-DATABASE_HOST='dpg-cma3qpi1hbls73ci605g-a.oregon-postgres.render.com'
-DATABASE_PORT='5432'
-
-
-
-API_URL = 'https://portfolio-backend-fdxe.onrender.com'
-DATABASE_URL= 'postgres://website_backend_0fvg_user:aL7a9VkJTAXsEXJbMcs89hrY55k1Xd1k@dpg-cma3qpi1hbls73ci605g-a.oregon-postgres.render.com/website_backend_0fvg'
-
 
 FRONT_END_DEVELOPMENT_URL='https://websitefront-git-dev-joaovitorwitts-projects.vercel.app/'
-PRODUCTION_URL='https://joaovitorwitt.com'
-
+DOMAIN_NAME='https://joaovitorwitt.com'
 
 ###################################################
 # Successful Responses
 ###################################################
 HTTP_OK_REQUEST = 200 # request succeeded depending of the HTTP method.
 HTTP_CREATED = 201 # Some resource was created, typically sent after a POST or a PUT request.
-
 
 ###################################################
 # Client Error Responses
@@ -39,14 +23,12 @@ HTTP_TOO_MANY_REQUESTS = 429 # client has sent too many requests in a given amou
 ###################################################
 HTTP_INTERNAL_SERVER_ERROR = 500 # server encountered a situation it does not know how to handle.
 
-
 ###################################################
 # database stuff
 ###################################################
 DEFAULT_POSTGRES_PORT=5432
 DEFAULT_POSTGRES_USER='postgres'
 DEFAULT_POSTGRES_PASSWORD='barezia12'
-
 
 ###################################################
 #  logging stuff
